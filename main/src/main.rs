2a3f858ab697b2c5e79fa75fa8498ac3ae274cb5
nguyenvankhuyen <nguyenvankhuyen@gapo.com.vn>
2020-12-22 10:27:22 +0700

    add test

--- main/src/main.rs
+++ main/src/main.rs
@@ -20,3 +20,31 @@ mod utils;
 async fn main() -> std::io::Result<()> {
     Server::run().await
 }
+
+mod tests {
+    use super::*;
+    use actix_web::{test, web, App};
+    use actix_web::test::read_response;
+
+    #[actix_rt::test]
+    async fn test_index_get() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(index_async))).await;
+        let req = test::TestRequest::with_header("content-type", "text/plain").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        let req1 = test::TestRequest::with_header("content-type", "text/plain").to_request();
+
+        let body = read_response(&mut app, req1).await;
+        println!("{:?}", resp);
+        assert_eq!(b"Hello world!\r\n", body.as_ref());
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn test_index_post() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(index_async))).await;
+        let req = test::TestRequest::post().uri("/").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        println!("{:?}", resp);
+        assert!(resp.status().is_client_error());
+    }
+}
